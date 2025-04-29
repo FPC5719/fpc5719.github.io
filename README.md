610d5a6cf28f482f88554dbc418063fdb4c3a17b
FPC5719 <fpc5719@163.com>
2025-04-29 14:39:26 +0000

    Basic Structures

---
+++ README.md
@@ -0,0 +1,7 @@
+个人博客。
+
+```
+nix build
+./result/bin/site build
+./result/bin/site server
+```
--- site.hs
+++ site.hs
@@ -4,8 +4,40 @@ import Hakyll
 
 main :: IO ()
 main = hakyll $ do
-  match "index.html" $ do
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
+
+  match "template/*" $ do
+    compile templateBodyCompiler
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ do
+      postTemplate <- loadBody "template/post.html"
+      pandocCompiler
+        >>= applyTemplate postTemplate postContext
+        >>= relativizeUrls
+
+  create ["index.html"] $ do
     route idRoute
     compile $ do
-      getResourceBody
+      postListContext' <- postListContext
+      indexTemplate    <- loadBody "template/index.html"
+      makeItem ""
+        >>= applyTemplate indexTemplate postListContext'
         >>= relativizeUrls
+
+postContext :: Context String
+postContext = mconcat
+  [ dateField "date" "%Y-%m-%d"
+  , defaultContext
+  ]
+
+postListContext :: Compiler (Context String)
+postListContext = do
+  posts <- loadAll "posts/*" >>= recentFirst
+  pure $ mconcat
+    [ listField "posts" postContext (return posts)
+    , defaultContext
+    ]
